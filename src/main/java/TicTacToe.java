46fceca967c6b48a45a12ac7c229f871ac72f57c
dyna2go <dynamine528@gmail.com>
2022-07-10 23:08:16 -0400

    This is the final code for Tiktaktoe. Alex Lee's tutorial was not working for some reason it has some slight bugs. Coding with Johns tutorial code is commented and pasted underneath. His code is working and showing X and O in the consoloe.

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,28 +1,62 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
+
+//This code does not seem to printing O for some reason I tried looking for the reason but I could not figure it out.
+//There are two programs in this final tic tac toe file
+//The active one is Alex Lee's it only prints X not O
+//The second one is Coding with John his prints both X and O
+
 public class TicTacToe {
+    static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
+    static ArrayList<Integer> cpuPositions = new ArrayList<Integer>();
+
     public static void main(String[] args) {
-        //Board
-        char[][] gameBoard = {
-                {' ', '|', ' ', '|', ' '},
+
+        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
                 {'-', '+', '-', '+', '-'},
                 {' ', '|', ' ', '|', ' '},
                 {'-', '+', '-', '+', '-'},
-                {' ', '|', ' ', '|', ' '},};
+                {' ', '|', ' ', '|', ' '}};
+
         printGameBoard(gameBoard);
 
         while (true) {
             Scanner scan = new Scanner(System.in);
             System.out.println("Enter your placement (1-9):");
             int playerPos = scan.nextInt();
+            while(playerPositions.contains(playerPos) || cpuPositions.contains(playerPositions)) {
+                System.out.println("Position taken! Enter a correct Position");
+                playerPos = scan.nextInt();
+            }
 
-            placePiece(gameBoard, playerPos, "player");
+
+            placePiece(gameBoard, playerPos,"player");
+
+            String result = checkWinner();
+            if(result.length() > 0) {
+                System.out.println(result);
+                break;
+            }
 
             Random rand = new Random();
             int cpuPos = rand.nextInt(9) + 1;
-            placePiece(gameBoard, cpuPos, "cpu");
+            while(playerPositions.contains(cpuPos) || cpuPositions.contains(cpuPos)) {
+                System.out.println("Position taken! Enter a correct Position");
+                cpuPos = rand.nextInt();
+            }
+            placePiece(gameBoard, cpuPos,"cpu");
 
             printGameBoard(gameBoard);
+
+            result = checkWinner();
+            System.out.println(result);
+            if(result.length() > 0){
+                System.out.println(result);
+                break;
+            }
         }
     }
     public static void printGameBoard(char[][] gameBoard){
@@ -30,15 +64,17 @@ public class TicTacToe {
             for(char c : row) {
                 System.out.print(c);
             }
-            System.out.println();
+           System.out.println();
         }
     }
     public static void placePiece(char[][] gameBoard, int pos, String user){
-       char symbol = ' ';
+        char symbol = ' ';
         if(user.equals("player")){
             symbol = 'X';
+            playerPositions.add(pos);
         } else if (user.equals("cpu")){
             symbol = 'O';
+            cpuPositions.add(pos);
         }
 
         switch(pos){
@@ -46,7 +82,7 @@ public class TicTacToe {
                 gameBoard[0][0] ='X';
                 break;
             case 2:
-                gameBoard[0][2] ='X';
+                gameBoard[0][1] = 'X';
                 break;
             case 3:
                 gameBoard[0][4] ='X';
@@ -73,4 +109,229 @@ public class TicTacToe {
                 break;
         }
     }
+    public static String checkWinner() {
+        List topRow = Arrays.asList(1, 2, 3);
+        List midRow = Arrays.asList(4, 5, 6);
+        List botRow = Arrays.asList(7, 8, 9);
+        List leftCol = Arrays.asList(1, 4, 7);
+        List midCol = Arrays.asList(2, 5, 8);
+        List rightCol = Arrays.asList(3, 6, 9);
+        List cross1 = Arrays.asList(1, 5, 9);
+        List cross2 = Arrays.asList(7, 5, 3);
+
+        List<List> winning = new ArrayList<List>();
+        winning.add(topRow);
+        winning.add(midRow);
+        winning.add(botRow);
+        winning.add(leftCol);
+        winning.add(midCol);
+        winning.add(rightCol);
+        winning.add(cross1);
+        winning.add(cross2);
+
+        for(List l : winning){
+            if(playerPositions.containsAll(l)) {
+                return "Congratulations you won!";
+            } else if(cpuPositions.contains(l)) {
+                return "CPU wins! Sorry :(";
+            } else if(playerPositions.size() + cpuPositions.size() == 9) {
+                return "CAT!";
+            }
+        }
+
+        return "";
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+/*
+//Coding with John
+//This youtubers code works
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class TicTacToe {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        char[][] board = {{' ', ' ', ' '},
+                {' ', ' ', ' '},
+                {' ', ' ', ' '}};
+
+        printBoard(board);
+
+        while (true) {
+            playerTurn(board, scanner);
+            if (isGameFinished(board)){
+                break;
+            }
+            printBoard(board);
+
+            computerTurn(board);
+            if (isGameFinished(board)){
+                break;
+            }
+            printBoard(board);
+        }
+        scanner.close();
+    }
+
+
+    private static boolean isGameFinished(char[][] board) {
+
+        if (hasContestantWon(board, 'X')) {
+            printBoard(board);
+            System.out.println("Player wins!");
+            return true;
+        }
+
+        if (hasContestantWon(board, 'O')) {
+            printBoard(board);
+            System.out.println("Computer wins!");
+            return true;
+        }
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        printBoard(board);
+        System.out.println("The game ended in a tie!");
+        return true;
+    }
+
+
+    private static boolean hasContestantWon(char[][] board, char symbol) {
+        if ((board[0][0] == symbol && board [0][1] == symbol && board [0][2] == symbol) ||
+                (board[1][0] == symbol && board [1][1] == symbol && board [1][2] == symbol) ||
+                (board[2][0] == symbol && board [2][1] == symbol && board [2][2] == symbol) ||
+
+                (board[0][0] == symbol && board [1][0] == symbol && board [2][0] == symbol) ||
+                (board[0][1] == symbol && board [1][1] == symbol && board [2][1] == symbol) ||
+                (board[0][2] == symbol && board [1][2] == symbol && board [2][2] == symbol) ||
+
+                (board[0][0] == symbol && board [1][1] == symbol && board [2][2] == symbol) ||
+                (board[0][2] == symbol && board [1][1] == symbol && board [2][0] == symbol) ) {
+            return true;
+        }
+        return false;
+    }
+
+
+    private static void computerTurn(char[][] board) {
+        Random rand = new Random();
+        int computerMove;
+        while (true) {
+            computerMove = rand.nextInt(9) + 1;
+            if (isValidMove(board, Integer.toString(computerMove))) {
+                break;
+            }
+        }
+        System.out.println("Computer chose " + computerMove);
+        placeMove(board, Integer.toString(computerMove), 'O');
+    }
+
+
+    private static boolean isValidMove (char[][] board, String position) {
+        switch(position) {
+            case "1":
+                return (board[0][0] == ' ');
+            case "2":
+                return (board[0][1] == ' ');
+            case "3":
+                return (board[0][2] == ' ');
+            case "4":
+                return (board[1][0] == ' ');
+            case "5":
+                return (board[1][1] == ' ');
+            case "6":
+                return (board[1][2] == ' ');
+            case "7":
+                return (board[2][0] == ' ');
+            case "8":
+                return (board[2][1] == ' ');
+            case "9":
+                return (board[2][2] == ' ');
+            default:
+                return false;
+        }
+    }
+
+    private static void playerTurn(char[][] board, Scanner scanner) {
+        String userInput;
+        while (true) {
+            System.out.println("Where would you like to play? (1-9)");
+            userInput = scanner.nextLine();
+            if (isValidMove(board, userInput)){
+                break;
+            } else {
+                System.out.println(userInput + " is not a valid move.");
+            }
+        }
+        placeMove(board, userInput, 'X');
+    }
+
+
+    private static void placeMove(char[][] board, String position, char symbol) {
+        switch(position) {
+            case "1":
+                board[0][0] = symbol;
+                break;
+            case "2":
+                board[0][1] = symbol;
+                break;
+            case "3":
+                board[0][2] = symbol;
+                break;
+            case "4":
+                board[1][0] = symbol;
+                break;
+            case "5":
+                board[1][1] = symbol;
+                break;
+            case "6":
+                board[1][2] = symbol;
+                break;
+            case "7":
+                board[2][0] = symbol;
+                break;
+            case "8":
+                board[2][1] = symbol;
+                break;
+            case "9":
+                board[2][2] = symbol;
+                break;
+            default:
+                System.out.println(":(");
+        }
+    }
+
+
+
+
+    private static void printBoard(char[][] board) {
+        System.out.println(board[0][0] + "|" +  board[0][1] + "|" +  board[0][2] );
+        System.out.println("-+-+-");
+        System.out.println(board[1][0] + "|" +  board[1][1] + "|" +  board[1][2] );
+        System.out.println("-+-+-");
+        System.out.println(board[2][0] + "|" +  board[2][1] + "|" +  board[2][2] );
+    }
+
+}
+
+ */
